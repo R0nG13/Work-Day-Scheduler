d5f1d7b64e3a363897bac08f7f6e489bc7e450c8
Ron G <Ron.Gould@gmail.com>
2020-09-06 18:25:50 -0400

    edit and save to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -14,21 +14,42 @@ Array.from(rows).forEach(row => {
   if (rowIdString) {
     rowHour = parseInt(rowIdString);
   }
+  console.log(rowHour)
+  
   if (rowHour) {
     // Compares row id to current hour and sets color accordingly
     if (currentHour === rowHour) {
-        $("span").addClass("present");
-    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-        $("span").addClass("future");
-    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-        $("span").addClass("past");
-    } else {
-      $("span").addClass("planning");
-
+        setColor(row, "red");
+      } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+        setColor(row, "green");
+      } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+        setColor(row, "lightgrey");
+      } else {
+        setColor(row, "lightyellow");
+      }
     }
+  });
+  
+  function setColor(element, color) {
+    element.style.backgroundColor = color;
   }
-});
 
-function setColor(element, color) {
-  element.style.backgroundColor = color;
-}
+
+
+
+
+  $(".saveBtn").click(function(e){
+    e.preventDefault()
+    console.log("clicked")
+    value = $(this).siblings("textarea").val()
+    textAreaId = $(this).siblings("textarea")[0].id
+    console.log(textAreaId)
+    localStorage.setItem(textAreaId, value)
+})
+
+$(".notes").each(function(){
+    console.log($(this)[0].id)
+    value = localStorage.getItem($(this)[0].id)
+    console.log(value)
+    $(this)[0].innerHTML = value
+})
