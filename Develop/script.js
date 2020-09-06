6de64bd9afb2b1bcd87d68b8fea72777bb56d696
Ron G <Ron.Gould@gmail.com>
2020-09-06 00:37:54 -0400

    added color change function

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,34 @@
 
 const today = moment();
 $("#currentDay").text(today.format("MMMM Do YYYY"));
+
+
+
+const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('H'));
+
+Array.from(rows).forEach(row => {
+  let
+    rowIdString = row.id,
+    rowHour;
+  if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+  }
+  if (rowHour) {
+    // Compares row id to current hour and sets color accordingly
+    if (currentHour === rowHour) {
+        $("span").addClass("present");
+    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+        $("span").addClass("future");
+    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+        $("span").addClass("past");
+    } else {
+      $("span").addClass("planning");
+
+    }
+  }
+});
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
